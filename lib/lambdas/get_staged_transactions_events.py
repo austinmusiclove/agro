9d3c406fd6f732ab6350ef43df446a174b5c8cc8
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-04-22 23:24:13 +0900

    Test message in lambda

--- lib/lambdas/get_staged_transactions_events.py
+++ lib/lambdas/get_staged_transactions_events.py
@@ -58,6 +58,7 @@ def get_staged_transactions_events(event, context):
         return {
             'statusCode': 200,
             'body': json.dumps({
+                'msg': 'win',
                 'count': len(serialized_records),
                 'transactions': serialized_records
             })
