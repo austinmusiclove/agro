cbbcabcdc6fb782f2f066064bbffd8ab6aec17ec
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-04-23 04:52:18 +0900

    Fixes event print

--- lib/lambdas/get_staged_transactions_events.py
+++ lib/lambdas/get_staged_transactions_events.py
@@ -58,7 +58,7 @@ def get_staged_transactions_events(event, context):
         return {
             'statusCode': 200,
             'body': json.dumps({
-                'event': event,
+                'event': json.dumps(event, default=str),
                 'path': event.get('path'),
                 'count': len(serialized_records),
                 'transactions': serialized_records
