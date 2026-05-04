8fa742daa9798758de94fd8129487d3d9796ee1c
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-05-05 02:46:26 +0900

    Adds current venue name

--- lib/mysql_interface/staged_transactions/get_staged_transactions.py
+++ lib/mysql_interface/staged_transactions/get_staged_transactions.py
@@ -13,10 +13,12 @@ def get_staged_transactions(connector, target_table):
             e_staged.venue_id as staged_venue_id,
             e_staged.start_date as staged_start_date,
             vs.name as staged_venue_name
+            vc.name as current_venue_name
         FROM staged_transactions st
         LEFT JOIN events e_current ON st.current_data_id = e_current.id
         LEFT JOIN events e_staged ON st.staged_data_id = e_staged.id
         LEFT JOIN venues vs ON e_staged.venue_id = vs.id
+        LEFT JOIN venues vs ON e_current.venue_id = vc.id
         WHERE st.target_table = ?
         AND st.status = 'pending-review';
     """
