04c1a51849acb3c55cdae943a699a1817abbad1f
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-05-15 11:50:56 +0200

    Fixes get next staged transaction

--- lib/mysql_interface/staged_transactions/get_next_staged_transaction.py
+++ lib/mysql_interface/staged_transactions/get_next_staged_transaction.py
@@ -4,11 +4,15 @@ def get_next_staged_transaction(connector, transaction_id):
         LEFT JOIN events e_staged ON st.staged_data_id = e_staged.id
         WHERE st.status = 'pending-review'
         AND (
-            e_staged.start_date > (SELECT e2.start_date FROM staged_transactions st2 LEFT JOIN events e2 ON st2.staged_data_id = e2.id WHERE st2.id = ?)
-            OR (e_staged.start_date = (SELECT e2.start_date FROM staged_transactions st2 LEFT JOIN events e2 ON st2.staged_data_id = e2.id WHERE st2.id = ?) AND st.id > ?)
+            e_staged.venue_id > (SELECT e2.venue_id FROM staged_transactions st2 LEFT JOIN events e2 ON st2.staged_data_id = e2.id WHERE st2.id = ?)
+            OR (e_staged.venue_id = (SELECT e2.venue_id FROM staged_transactions st2 LEFT JOIN events e2 ON st2.staged_data_id = e2.id WHERE st2.id = ?)
+                AND e_staged.start_date > (SELECT e2.start_date FROM staged_transactions st2 LEFT JOIN events e2 ON st2.staged_data_id = e2.id WHERE st2.id = ?))
+            OR (e_staged.venue_id = (SELECT e2.venue_id FROM staged_transactions st2 LEFT JOIN events e2 ON st2.staged_data_id = e2.id WHERE st2.id = ?)
+                AND e_staged.start_date = (SELECT e2.start_date FROM staged_transactions st2 LEFT JOIN events e2 ON st2.staged_data_id = e2.id WHERE st2.id = ?)
+                AND st.id > ?)
         )
         ORDER BY e_staged.venue_id, e_staged.start_date ASC, st.id ASC
         LIMIT 1
     """
-    result = connector.execute_query(sql, [transaction_id, transaction_id, transaction_id])
+    result = connector.execute_query(sql, [transaction_id, transaction_id, transaction_id, transaction_id, transaction_id])
     return result[0].get('id') if result else None
